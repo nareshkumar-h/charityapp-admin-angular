dbaa6c4a408c7eda449ecd7e578aa4d5b9e7d964
Keyneloui <keyne.loui@gmail.com>
2019-11-06 12:03:23 +0530

    added

--- src/app/service/adminservice.service.ts
+++ src/app/service/adminservice.service.ts
@@ -10,13 +10,8 @@ export class AdminserviceService {
     private http:HttpClient
   ) { }
 
-<<<<<<< HEAD
-  //apiUrl='https://charity-requestor.herokuapp.com/';
-  apiUrl='https://charityapp-api-gateway.herokuapp.com/requestapp/';
-=======
   apiUrl='https://charity-requestor.herokuapp.com/';
   //apiUrl='https://peaceful-scrubland-89780.herokuapp.com/requestapp/';
->>>>>>> 28c4fb92cc4b9cd08d24a19e611d595b7b16aea5
 
   addFundRequest(formData:any):any
   {
